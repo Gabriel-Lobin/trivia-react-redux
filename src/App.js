85764f82430659ac143021d29f310233aeb221f5
Alessandro Hugen <alessandrohugen@gmail.br>
2021-09-02 18:15:02 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Game from './pages/Game';
 import Login from './pages/Login';
+import Config from './pages/Config';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
       <Route path="/game" component={ Game } />
+      <Route exact path="/config" component={ Config } />
     </Switch>
   );
 }
--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,14 +1,39 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 
 class Game extends React.Component {
+  constructor() {
+    super();
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const { history } = this.props;
+    history.push('/config');
+  }
+
   render() {
     return (
       <div>
         Game Page
+        <button
+          type="button"
+          data-testid="btn-settings"
+          onClick={ this.handleClick }
+        >
+          Configurações
+        </button>
       </div>
     );
   }
 }
 
-export default connect()(Game);
+Game.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default connect(null, null)(Game);
