bccdfefa3772ae6dc04573330bc674c6d44b2046
Genesis-Henriques <GenesisHenriques@gmail.com>
2021-09-03 14:32:41 -0300

    Requisito 4 pronto

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import GameHeader from '../components/GameHeader';
 
 class Game extends React.Component {
   render() {
     return (
       <div>
+        <header><GameHeader /></header>
         Game Page
       </div>
     );
