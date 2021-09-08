61d8ab689e5777989bf795550ffb8fcdfde08439
Jacson Rodrigues <jacson.s.r1@gmail.com>
2021-09-08 18:38:17 -0300

    Atualiza Requisito 11 para incluit o 14

--- src/pages/Feedback/index.js
+++ src/pages/Feedback/index.js
@@ -7,14 +7,19 @@ const BASE_SCORE = 3;
 
 class Feedback extends React.Component {
   render() {
-    const { questions, assertions } = this.props;
+    const { questions, assertions, score } = this.props;
     const feedbackText = assertions >= BASE_SCORE ? 'Mandou bem!' : 'Podia ser melhor...';
     return (
       <>
         <GameHeader />
         <main className="feedback">
           <h1 data-testid="feedback-text">{feedbackText}</h1>
-          <h2 data-testid="feedback-total-score">
+          <h2>
+            Você marcou
+            {' '}
+            <span data-testid="feedback-total-score">{score}</span>
+          </h2>
+          <h2>
             Você acertou
             {' '}
             <span data-testid="feedback-total-question">{assertions}</span>
@@ -31,9 +36,11 @@ Feedback.propTypes = {
   questions: PropTypes.shape({
     length: PropTypes.number.isRequired,
   }).isRequired,
+  score: PropTypes.number.isRequired,
 };
 
 const mapStateToProps = (state) => ({
+  score: state.player.score,
   assertions: state.player.assertions,
   questions: state.questions.data,
 });
