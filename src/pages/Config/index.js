ae3ea7e7de23b04a0ab1309608040271c3ab992c
gabriel suassuna amorim <gabrielsuassuna@hotmail.com>
2021-09-11 10:20:54 -0300

    settingsBtn rotate e bug nextBtn não aparece ao acabar o tempo

--- src/pages/Config/index.js
+++ src/pages/Config/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Config extends React.Component {
   render() {
     return (
-      <h1 data-testid="settings-title">Configurações</h1>
+      <div>
+        <h1 data-testid="settings-title">Configurações</h1>
+        <Link to="/">Login</Link>
+      </div>
     );
   }
 }
