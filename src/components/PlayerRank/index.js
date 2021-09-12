d8f20e2cca1616d633398695d3e8cbc7c59fdeb9
Genesis-Henriques <GenesisHenriques@gmail.com>
2021-09-12 17:38:47 -0300

    Style da pagina de ranking

--- src/components/PlayerRank/index.js
+++ src/components/PlayerRank/index.js
@@ -1,15 +1,22 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import './style.css';
 
 export default class PlayerRank extends Component {
   render() {
-    const { player, index } = this.props;
+    const { player, index, ranking } = this.props;
     const { name, score, picture } = player;
     return (
-      <div>
-        <img src={ picture } alt="Icone do jogador" />
-        <h3 data-testid={ `player-name-${index}` }>{name}</h3>
-        <h3 data-testid={ `player-score-${index}` }>{`${score} pontos`}</h3>
+      <div className="player">
+        <img src={ picture } alt="Icone do jogador" className="player-icon" />
+        <h3 data-testid={ `player-name-${index}` } className="player-name">{name}</h3>
+        <h3
+          data-testid={ `player-score-${index}` }
+          className="player-score"
+        >
+          {`${score} pontos`}
+        </h3>
+        <img src={ ranking } alt="Ranking do jogador" className="player-ranking" />
       </div>
     );
   }
@@ -18,4 +25,5 @@ export default class PlayerRank extends Component {
 PlayerRank.propTypes = {
   index: PropTypes.number.isRequired,
   player: PropTypes.func.isRequired,
-};
+  ranking: PropTypes.number.isRequired,
+}
